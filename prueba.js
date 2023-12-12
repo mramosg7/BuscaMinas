a27f7557224841111ecc332f371f771c25d529db
Mario Ramos García <93348244+mramosg7@users.noreply.github.com>
2023-12-12 13:38:25 +0100

    Delete prueba.js

--- prueba.js
+++
@@ -1,7 +0,0 @@
-let table= document.createElement('table');
-let td= document.createElement('td');
-let tr= document.createElement('tr');
-td.textContent("hola");
-tr.appendChild(td);
-table.appendChild(tr);
-document.body.appendChild(table);
